10a6c6bd1e90380470ff60f47e3538668b826d2f
None <george_vibram@yeah.net>
2016-11-17 21:53:37 +0800

    默认配置文件

---
+++ core/config/config.php
@@ -0,0 +1,15 @@
+<?php
+	return array(
+		'CONTRL' => 'index',
+		'ACTION' => 'da',
+		
+		'database_type' => 'mysql',
+		'database_name' => 'test',
+		'server' => 'localhost',
+		'username' => 'root',
+		'password' => '',
+		'charset' => 'utf8',
+		
+		'OPTION' => 'file',
+	);
+?>
